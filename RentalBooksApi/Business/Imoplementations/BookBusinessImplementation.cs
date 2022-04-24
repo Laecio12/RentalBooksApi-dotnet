773a941b162ea5171785bd849a5c8df04a682dcf
laecio silva <laecio12silva@gmail.com>
2022-04-24 14:04:49 -0300

    feat: adicionando a implementação de BookBusiness

---
+++ RentalBooksApi/Business/Imoplementations/BookBusinessImplementation.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using RentalBooksApi.Data.Covert.Implementation;
+using RentalBooksApi.Data.Vo;
+using RentalBooksApi.Models;
+using RentalBooksApi.Repository.Generic;
+
+namespace RentalBooksApi.Business.Imoplementations
+{
+    public class BookBusinessImplementation : IBookBusiness
+    {
+        private readonly IRepository<Book> _repository;
+        private readonly BookConvert _convert;
+
+        public BookBusinessImplementation(IRepository<Book> repository)
+        {
+            _repository = repository;
+            _convert = new BookConvert();
+        }
+        public BookVo Create(BookVo book)
+        {
+            var bookEntity = _convert.Parse(book);
+            bookEntity = _repository.Create(bookEntity);
+            return _convert.Parse(bookEntity);
+        }
+
+        public BookVo FindById(long id)
+        {
+            return _convert.Parse(_repository.FindById(id));
+        }
+
+        public List<BookVo> FindAll()
+        {
+            return _convert.Parse(_repository.FindAll());
+        }
+
+        public BookVo Update(BookVo book)
+        {
+            var bookEntity = _convert.Parse(book);
+            bookEntity = _repository.Create(bookEntity);
+            return _convert.Parse(bookEntity);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
